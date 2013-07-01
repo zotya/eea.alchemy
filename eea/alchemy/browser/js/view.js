79b29ed0ecccbbd37301d1162cbb3886e0406819
Alin Voinea <alin@localhost.localdomain>
2013-07-01 18:35:10 +0300

    Refs #14797 - Add custom JS EEA.Highlighter
    - Add custom JS Highlighter in order to match our needs

--- eea/alchemy/browser/js/view.js
+++ eea/alchemy/browser/js/view.js
@@ -16,7 +16,8 @@ EEA.Highlighter = function(context, options){
   self.context = context;
 
   self.settings = {
-    highlightClass: 'eea-alchemy-tag'
+    highlightClass: 'eea-alchemy-tag',
+    caseInsensitive: true
   };
 
   if(options){
@@ -28,7 +29,52 @@ EEA.Highlighter = function(context, options){
 
 EEA.Highlighter.prototype = {
   initialize: function(){
+    var self = this;
+    self.highlight(self.context);
+  },
 
+  highlight: function(startnode) {
+    var self = this;
+    if(!startnode.length){
+      return;
+    }
+
+    jQuery.each(self.settings.search, function(key, value) {
+      startnode.find('*:not(textarea)').andSelf().contents().each(function() {
+        if (this.nodeType === 3) {
+          self.highlightTermInNode(this, key, value);
+        }
+      });
+    });
+  },
+
+  highlightTermInNode: function(node, word, link){
+    var self = this;
+    var highlight, index, next;
+    var c = node.nodeValue;
+
+    var s1 = '\\s[\\"\\{\\[\\(]?';
+    var s2 = '[\\"\\}\\]\\)\\.\\,\\!\\?\\;\\:\\s]';
+    var search = new RegExp(s1 + word + s2, 'gi');
+
+    // Highlight
+    highlight = function(content, word, url) {
+      return jQuery('<span class="' + self.settings.highlightClass + '" data-word="' + word + '" data-url="' + url + word + '">' + content + '</span>');
+    };
+
+    while (c && c.search(search) > -1) {
+        // replace the node with [before]<span>word</span>[after]
+        index = c.toLowerCase().indexOf(word.toLowerCase());
+        jQuery(node)
+            .before(document.createTextNode(c.substr(0, index)))
+            .before(highlight(c.substr(index, word.length), word, link))
+            .before(document.createTextNode(c.substr(index+word.length)));
+        next = node.previousSibling; // text after the a
+        jQuery(node).remove();
+        // wash, rinse and repeat
+        node = next;
+        c = node.nodeValue;
+    }
   }
 };
 
@@ -85,37 +131,47 @@ EEA.Alchemy.prototype = {
 
   reload: function(){
     var self = this;
+    self.exists = {};
     var adapter = new EEA.Highlighter(self.context, self.settings);
     self.context.data('EEAHighlighter', adapter);
-    //jQuery('.' + self.settings.highlightClass, self.context).each(function(){
-      //return self.reloadItem(jQuery(this));
-    //});
+    jQuery('.' + self.settings.highlightClass, self.context).each(function(){
+      return self.reloadItem(jQuery(this));
+    });
+  },
+
+  reloadItem: function(item){
+    var self = this;
+
+    // Skip blacklisted items
+    var blacklist = self.settings.blacklist || [];
+    blacklist.push('a');
+    blacklist = blacklist.join(',');
+
+    if(item.parents(blacklist).length){
+      item.removeClass(self.settings.highlightClass);
+      return;
+    }
+
+    var url = item.data('url');
+    if(!url){
+      return;
+    }
+
+    // Handle Highlight firstOnly option
+    var word = item.data('word');
+    if(self.settings.firstOnly && self.exists[word]){
+      item.removeClass(self.settings.highlightClass);
+      return;
+    }
+    self.exists[word] = url;
+
+    // Add link
+    var link = jQuery('<a>')
+      .attr('href', url)
+      .text(item.text());
+    item.html(link);
+    item.show('highlight', 2000);
   }
-
-  //reloadItem: function(item){
-    //var self = this;
-
-    //// Skip blacklisted items
-    //var blacklist = self.settings.blacklist || [];
-    //blacklist.push('a');
-    //blacklist = blacklist.join(',');
-
-    //if(item.parents(blacklist).length){
-      //item.removeClass(self.settings.highlightClass);
-      //return;
-    //}
-
-    //var url = self.settings.search[item.text()];
-    //if(!url){
-      //return;
-    //}
-
-    //var link = jQuery('<a>')
-      //.attr('href', url + item.text())
-      //.text(item.text());
-    //item.html(link);
-    //item.show('highlight', 2000);
-  //}
 };
 
 jQuery.fn.EEAlchemy = function(options){
@@ -138,6 +194,17 @@ jQuery(document).ready(function(){
     return;
   }
 
+  // Skip edit forms
+  var href = window.location.href.split('?')[0];
+  if(href.endswith('edit')){
+    return;
+  }
+
+  if(items.find('[name="edit_form"]').length){
+    return;
+  }
+
+  // Compute base link
   base = jQuery('base').attr('href') || '';
 
   if(base && base.endswith('/view')){
